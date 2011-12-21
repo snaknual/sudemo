0ec1bb7484bbb4090eb62d47d0e9c514a3327b98
Su Naknual <sudteerak@gmail.com>
2011-12-20 19:00:35 -0500

    adding content files for sume site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sudemo::Application.config.secret_token = '209782a2c09b95230b88480689e9361ac285e299bc6862ee6a3cac9e6225110f723244b9e3f7155126b5a4079bd00bb3974c7929267d7ed58f97a9bf5cfdcd98'
